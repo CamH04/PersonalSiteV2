b3ae2b28202f353e7ea0d63507fd5bc2f0dd368b
Cam <cameronahaynes04@gmail.com>
2023-05-11 12:57:48 +0100

    Added content to contact page

--- site/src/pages/Contact.js
+++ site/src/pages/Contact.js
@@ -1,6 +1,25 @@
+import './Contact.css';
+
 export default function Contact (){
     return(
         <section>
+            <div class="grid">
+            <div class="col-7 col-6-m col-3-s">
+                <h1>Hello!</h1>
+                <p class="big">
+                I would love to hear from you! If you wish to contact me you can use the options below. I've put a lot of effort into this site and hope you enjoy it as much as i do!
+                </p>
+            </div>
+            <div class="col-5 hide-m"></div>
+            <div class="col-4 col-3-m col-3-s">
+                <p class="light">Send me an email</p>
+                <p class="big"><a href="mailto:wouterdebres@gmail.com">cameronahaynes04@gmail.com</a></p>
+            </div>
+            <div class="col-5 col-3-m col-3-s">
+                <p class="light">Connect on LinkedIn</p>
+                <p class="big"><a href="https://www.linkedin.com/in/cameron-haynes-778056233/">linkedin.com/cameron-haynes/</a></p>
+            </div>
+            </div>
         </section>
     );
 }
