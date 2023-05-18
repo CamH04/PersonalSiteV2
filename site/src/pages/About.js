1ba3c2d9165dc6c903daee3fff2ea58f47eaec05
Cam <cameronahaynes04@gmail.com>
2023-05-18 14:53:14 +0100

    Changes to about page

--- site/src/pages/About.js
+++ site/src/pages/About.js
@@ -10,7 +10,7 @@ export default function About (){
             <div class="grid">
                 <div class="col-1 hide-m"></div>
                 <div class="col-4 col-5-m col-3-s" >
-                    <h1>Cameron Haynes -<span class="light">Dedicated Coder and Tech Nerd</span></h1> 
+                    <h1>Cameron Haynes -<span class="light">Dedicated Coder and Music Lover</span></h1> 
                     <ul class="contact">
                     <li><Link to="/contact" class="button">Get in touch</Link></li>
                     <li><a href="https://github.com/CamH04" target="_blank" class="social"><img src={GitHubLogo}alt="GitHub" width="30px" height="30px"></img></a></li>
@@ -19,16 +19,19 @@ export default function About (){
                 </div>
                 <div class="col-6 col-4-m col-3-s">
                     <p class="big">
-                    I am the father of the most beautiful twin daughters in the world. I live in the Netherlands and I love to ride my motorcyle to unwind.
+                    I'm a programmer from Hampshire who is currently based in Sunderland. I love to listen and create music to unwind.
                     </p>
                     <p class="big">
-                    A long time ago I started my career as a psychologist, but quickly realized that designing digital products is my true passion. My master's in psychology turned out to be a tremendous help with understanding human behavior and how to best design for it; <i>simplicity is the key to great user experiences.</i>
+                    I started programming in senior school and even back then i was programming heavily in my spare time. I learnt more and more, leading to where i am now. I love to experment with code and build a large varity of projects using any tech i can get my hands on.
                     </p>
                     <p class="big">
-                    I have around 16 years experience designing digital products. I co-founded two companies called Bread&amp;Pepper and Gibbon. The last one got acquired by Degreed, and for several years I have enjoyed leading and growing the product design team of this edtech unicorn.
+                    I love creating website which are intuitve to the user, creating an efficent environment for those to utilise 
                     </p>
                     <p class="big">
-                    I love simplicity, have an allergy for bullsh*t and always strive to be a good human. If you think I can help you with any project or coaching, don't hesitate to get in touch.
+                    I love creating website which are intuitve to the user, creating an efficent environment for those to utilise 
+                    </p>
+                    <p class="big">
+                    Im experianced in C# and the 3 expected languages (HTML/CSS/JavaScript) with React and Blazor being my Web Frameworks of choice
                     </p>
                 </div>
             </div>
