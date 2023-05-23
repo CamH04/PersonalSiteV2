5c39d3140bf708927fe03f76b3ce6e56462868de
Cam <cameronahaynes04@gmail.com>
2023-05-23 23:06:28 +0100

    Non Live Changes

--- site/src/pages/About.js
+++ site/src/pages/About.js
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom"
 import { Link } from 'react-router-dom';
 import GitHubLogo from './img/GitHubLogo.png';
 import LinkInLogo from './img/LinkInLogo.png';
+import Gif1 from './img/1.gif';
 
 export default function About (){
     return(
@@ -16,23 +17,27 @@ export default function About (){
                     <li><a href="https://github.com/CamH04" target="_blank" class="social"><img src={GitHubLogo}alt="GitHub" width="30px" height="30px"></img></a></li>
                     <li><a href="https://www.linkedin.com/in/cameron-haynes-778056233/" target="_blank" class="social"><img src={LinkInLogo} alt="LinkedIn" width="30px" height="30px"></img></a></li>
                     </ul>
+                    <img id="gif" src={Gif1}></img>
                 </div>
                 <div class="col-6 col-4-m col-3-s">
                     <p class="big">
-                    I'm a programmer from Hampshire who is currently based in Sunderland. I love to listen and create music to unwind.
+                    Hi! I'm Cameron, a programmer from Hampshire who is currently based in Sunderland. I love to create and listen to music in my off time.
                     </p>
                     <p class="big">
                     I started programming in senior school and even back then i was programming heavily in my spare time. I learnt more and more, leading to where i am now. I love to experment with code and build a large varity of projects using any tech i can get my hands on.
                     </p>
                     <p class="big">
-                    I love creating website which are intuitve to the user, creating an efficent environment for those to utilise 
-                    </p>
-                    <p class="big">
-                    I love creating website which are intuitve to the user, creating an efficent environment for those to utilise 
+                    I love creating website which are intuitve to the user, creating a nice environment for them to use 
                     </p>
                     <p class="big">
                     Im experianced in C# and the 3 expected languages (HTML/CSS/JavaScript) with React and Blazor being my Web Frameworks of choice
                     </p>
+                    <p class="big">
+                    Tinkering around with software is fun but i also love to exepriment with hardware and electronics using Ardunio's and Esp's
+                    </p>
+                </div>
+                <div class="col-12">
+                    <h1 class="help"> TEST </h1>
                 </div>
             </div>
         </section>
