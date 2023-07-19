579687d9c028563e9130a7e43e4a536b1e471821
Cam <cameronahaynes04@gmail.com>
2023-07-19 14:16:35 +0100

    all animations added

--- site/src/pages/About.js
+++ site/src/pages/About.js
@@ -5,21 +5,29 @@ import GitHubLogo from './img/GitHubLogo.png';
 import LinkInLogo from './img/LinkInLogo.png';
 import ElecGif from './img/gif.webp'
 
+import React, { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+
 export default function About (){
+    useEffect(() => {
+        Aos.init();
+      });
+
     return(
         <section class="haze">
             <div class="grid">
                 <div class="col-1 hide-m"></div>
                 <div class="col-4 col-5-m col-3-s">
-                    <h1>Cameron Haynes -<span class="light">Dedicated Coder and Music Lover</span></h1> 
-                    <ul class="contact">
-                    <li><Link to="/contact" class="button">Get in touch</Link></li>
-                    <li><a href="https://github.com/CamH04" target="_blank" class="social"><img src={GitHubLogo}alt="GitHub" width="30px" height="30px"></img></a></li>
-                    <li><a href="https://www.linkedin.com/in/cameron-haynes-778056233/" target="_blank" class="social"><img src={LinkInLogo} alt="LinkedIn" width="30px" height="30px"></img></a></li>
-                    <img class="help gif" src={ElecGif} width="150px" height="150px"></img>
+                    <h1 data-aos="fade-up" data-aos-duration="1500" >Cameron Haynes -<span class="light">Dedicated Coder and Music Lover</span></h1> 
+                    <ul class="contact" data-aos="zoom-in-up" data-aos-duration="1000">
+                    <li data-aos="zoom-in-up" data-aos-duration="1000" ><Link to="/contact" class="button">Get in touch</Link></li>
+                    <li data-aos="zoom-in-up" data-aos-duration="1000" ><a href="https://github.com/CamH04" target="_blank" class="social"><img src={GitHubLogo}alt="GitHub" width="30px" height="30px"></img></a></li>
+                    <li data-aos="zoom-in-up" data-aos-duration="1000" ><a href="https://www.linkedin.com/in/cameron-haynes-778056233/" target="_blank" class="social"><img src={LinkInLogo} alt="LinkedIn" width="30px" height="30px"></img></a></li>
+                    <img data-aos="zoom-in-up" data-aos-duration="1000" class="help gif" src={ElecGif} width="150px" height="150px"></img>
                     </ul>
                 </div>
-                <div class="col-6 col-4-m col-3-s">
+                <div class="col-6 col-4-m col-3-s" data-aos="zoom-in-left" data-aos-duration="1200" >
                     <p class="big">
                     Hi! I'm Cameron, a programmer from Hampshire who is currently based in Sunderland. I love to create and listen to music in my off time.
                     </p>
